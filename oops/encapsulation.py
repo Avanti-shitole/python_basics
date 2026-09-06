0d7f044d9e1d35a2fa09a5b95e9a3495cddaa65e
Avanti-shitole <avantishitole974@gmail.com>
2026-09-06 14:51:40 +0530

    encapsulation example

--- oops/encapsulation.py
+++ oops/encapsulation.py
@@ -2,10 +2,14 @@ class BankAccount:
     def __init__(self, balance):
         self.__balance = balance
 
+    def deposit(self, amount):
+        self.__balance += amount
+
     def show_balance(self):
         print("Balance:", self.__balance)
 
 
 account = BankAccount(5000)
 
+account.deposit(1000)
 account.show_balance()
