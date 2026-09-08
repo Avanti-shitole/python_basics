13ee53ed27789ed2a716d99b09eb7ae4b2ff91c5
Avanti-shitole <avantishitole974@gmail.com>
2026-09-08 16:04:01 +0530

    Add programs

---
+++ functions/parameters_return.py
@@ -0,0 +1,21 @@
+# Addition using parameters and return
+
+def add(a, b):
+    return a + b
+
+
+result = add(10, 20)
+print("Addition:", result)
+
+
+# Even or Odd using parameter
+
+def even_odd(num):
+    if num % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+
+result = even_odd(10)
+print(result)
